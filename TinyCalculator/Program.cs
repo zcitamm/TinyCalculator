e31936bc9f2c1aa7cdcc13d896bd0547e962417d
Johannes Egger <eggj@htlvb.at>
2023-09-29 09:16:36 +0200

    Extract ReadNumber

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,33 +1,24 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
-int dividend;
-while (true)
-{
-    Console.Write("Dividend: ");
-    string input = Console.ReadLine();
-    if (int.TryParse(input, out dividend))
-    {
-        break;
-    }
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"{input} is not a number.");
-    Console.ResetColor();
-}
-
-int divisor;
-while (true)
-{
-    Console.Write("Divisor: ");
-    string input = Console.ReadLine();
-    if (int.TryParse(input, out divisor))
-    {
-        break;
-    }
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"{input} is not a number.");
-    Console.ResetColor();
-}
+int dividend = ReadNumber("Dividend");
+int divisor = ReadNumber("Divisor");
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
 Console.ResetColor();
+
+static int ReadNumber(string name)
+{
+    while (true)
+    {
+        Console.Write($"{name}: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int dividend))
+        {
+            return dividend;
+        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{input} is not a number.");
+        Console.ResetColor();
+    }
+}
