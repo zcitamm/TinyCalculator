aa1b80d460cd474df0a0a336a83155cba6d0174b
Matic Ivan <Ivan.MATIC@htlvb.at>
2023-11-10 09:17:00 +0100

    Choose operator for calculation

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,9 +1,39 @@
-﻿Console.WriteLine("== Tiny calculator ==");
+﻿using System.Xml.Linq;
 
-int dividend = ReadNumber("Dividend");
-int divisor = ReadNumber("Divisor");
+Console.WriteLine("== Tiny calculator ==");
 
-if (divisor == 0)
+
+int firstNumber = 0;
+int secondNumber = 0;
+int result = 0;
+
+char op = ChooseOperator();
+
+switch (op)
+{
+    case '+':
+        firstNumber = ReadNumber("Summand");
+        secondNumber = ReadNumber("Summand");
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        firstNumber = ReadNumber("Minuend");
+        secondNumber = ReadNumber("Subtrahend");
+        result = firstNumber - secondNumber;
+        break;
+    case '/':
+        firstNumber = ReadNumber("Multiplikator");
+        secondNumber = ReadNumber("Multiplikand");
+        result = firstNumber * secondNumber;
+        break;
+    case '*':
+        firstNumber = ReadNumber("Dividend");
+        secondNumber = ReadNumber("Divisor");
+        result = firstNumber / secondNumber;
+        break;
+}
+
+if (secondNumber == 0 && op == '/')
 {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("Divisor darf nicht 0 sein.");
@@ -12,8 +42,24 @@ if (divisor == 0)
 else
 {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
+    Console.WriteLine($"{firstNumber} {op} {secondNumber} = {result}");
     Console.ResetColor();
+}
+
+static char ChooseOperator()
+{
+    while (true)
+    {
+        Console.Write($"Choose an operator[+, -, *, /]: ");
+        string operand = Console.ReadLine();
+        if (operand == "+" || operand == "-" || operand == "*" || operand == "/")
+        {
+            return char.Parse(operand);
+        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{operand} is not a operator [+, -, *, /].");
+        Console.ResetColor();
+    }
 }
 
 static int ReadNumber(string name)
@@ -31,3 +77,9 @@ static int ReadNumber(string name)
         Console.ResetColor();
     }
 }
+
+
+
+
+
+
