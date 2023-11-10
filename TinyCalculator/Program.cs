4843f58cae269c834827cb23cdaee8e43b24f25c
Matic Ivan <Ivan.MATIC@htlvb.at>
2023-11-10 09:19:57 +0100

    Continue calculation with the result of the previous calculation

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,20 +1,30 @@
-﻿Console.WriteLine("== Tiny calculator ==");
+﻿using System.Xml.Linq;
+
+Console.WriteLine("== Tiny calculator ==");
 
 int dividend = ReadNumber("Dividend");
 int divisor = ReadNumber("Divisor");
 
-if (divisor == 0)
+while (true)
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Divisor darf nicht 0 sein.");
-    Console.ResetColor();
+    if (divisor == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Divisor darf nicht 0 sein.");
+        Console.ResetColor();
+        divisor = ReadNumber("Divisor");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
+        Console.ResetColor();
+        dividend = dividend / divisor;
+        Console.WriteLine($"Dividend: {dividend}");
+        divisor = ReadNumber("Divisor");
+    }
 }
-else
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
-    Console.ResetColor();
-}
+
 
 static int ReadNumber(string name)
 {
