35b94c2696cb388dedff1f156e9a3fd2a965f214
Ivanovtek <85580228+Ivanovtek@users.noreply.github.com>
2023-11-17 09:20:54 +0100

    Added raising numbers to the power of

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -9,70 +9,65 @@ using System;
             double firstNumber = ReadNumber("Erste Nummer");
             double secondNumber = ReadNumber("Zweite Nummer");
 
-            if (secondNumber == 0)
+            char op = ChooseOperator();
+
+            switch (op)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Divisor darf nicht 0 sein.");
-                Console.ResetColor();
-                secondNumber = ReadNumber("Divisor");
-            }
-            else
-            {
-                char op = ChooseOperator();
-
-                switch (op)
-                {
-                    case '+':
-                        //secondNumber = ReadNumber("Summand");
-                        firstNumber = firstNumber + secondNumber;
-                        break;
-                    case '-':
-                        //secondNumber = ReadNumber("Subtrahend");
-                        firstNumber = firstNumber - secondNumber;
-                        break;
-                    case '*':
-                        //secondNumber = ReadNumber("Multiplikand");
-                        firstNumber = firstNumber * secondNumber;
-                        break;
-                    case '/':
-                        //secondNumber = ReadNumber("Divisor");
-                        if (secondNumber == 0)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Divisor darf nicht 0 sein.");
-                            Console.ResetColor();
-                            continue; // Skip the current iteration and ask for input again
-                        }
-                        firstNumber = firstNumber / secondNumber;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid operator. Please choose again.");
-                        continue; // Skip the current iteration and ask for input again
-                }
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Result: {firstNumber.ToString("F")}");
-                Console.ResetColor();
-
-                Console.Write("Do you want to continue? (y/n): ");
-                string continueInput = Console.ReadLine();
-                if (continueInput.ToLower() != "y")
+                case '+':
+                    //secondNumber = ReadNumber("Summand");
+                    firstNumber = firstNumber + secondNumber;
                     break;
+                case '-':
+                    //secondNumber = ReadNumber("Subtrahend");
+                    firstNumber = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    //secondNumber = ReadNumber("Multiplikand");
+                    firstNumber = firstNumber * secondNumber;
+                    break;
+                case '^':
+                    firstNumber = Math.Pow(firstNumber, secondNumber);
+                    break;
+                case '/':
+                    //secondNumber = ReadNumber("Divisor");
+                    if (secondNumber == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Divisor darf nicht 0 sein.");
+                        Console.ResetColor();
+                        continue; // Skip the current iteration and ask for input again
+                    }
+                    firstNumber = firstNumber / secondNumber;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator. Please choose again.");
+                    continue; // Skip the current iteration and ask for input again
             }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Result: {firstNumber.ToString("F")}");
+            Console.ResetColor();
+
+            Console.Write("Do you want to continue? (y/n): ");
+            string? continueInput = Console.ReadLine();
+            if (continueInput is not null && continueInput.ToLower() != "y")
+                break;
         }
+
+
 
     static char ChooseOperator()
     {
         while (true)
         {
-            Console.Write($"Choose an operator [+ - * /]: ");
+            Console.Write($"Choose an operator [+ - * / ^]: ");
             string operand = Console.ReadLine();
-            if (operand.Length == 1 && "+-*/".Contains(operand))
+            if (operand.Length == 1 && "+-*/^".Contains(operand))
             {
                 return operand[0];
             }
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{operand} is not a valid operator [+ - * /].");
+            Console.WriteLine($"{operand} is not a valid operator [+ - * / ^].");
             Console.ResetColor();
         }
     }
